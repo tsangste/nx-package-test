feba4285121cf89b11410c5d02d463047e6559ae
Steven Tsang <tsangste@gmail.com>
2022-10-06 13:21:15 +0100

    format changes in release.config.base.js

--- release.config.base.js
+++ release.config.base.js
@@ -1,8 +1,8 @@
 module.exports = {
   branches: [
-    {name: 'master'},
-    {name: 'release/*', channel: 'beta', prerelease: 'beta'},
-    {name: 'develop', channel: 'alpha', prerelease: 'alpha'},
+    { name: 'master' },
+    { name: 'release/*', channel: 'beta', prerelease: 'beta' },
+    { name: 'develop', channel: 'alpha', prerelease: 'alpha' },
   ],
   extends: 'semantic-release-npm-github-publish',
 }
