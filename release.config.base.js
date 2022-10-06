1ef70dc0dc0331f7156aeb1f8d8b5f91b1760ae5
Steven Tsang <tsangste@gmail.com>
2022-10-06 13:15:22 +0100

    set branch channels

--- release.config.base.js
+++ release.config.base.js
@@ -1,3 +1,8 @@
 module.exports = {
+  branches: [
+    {name: 'master'},
+    {name: 'release/*', channel: 'beta', prerelease: 'beta'},
+    {name: 'develop', channel: 'alpha', prerelease: 'alpha'},
+  ],
   extends: 'semantic-release-npm-github-publish',
 }
