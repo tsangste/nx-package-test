a510e202d4d27b16fbdab7b442ee7844ce09aaf1
Steven Tsang <tsangste@gmail.com>
2023-01-09 10:16:04 +0000

    chore(auth): rename package name from @tsangste/auth to @tsangste/nestjs-auth

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
 
-import { AuthModule } from '@tsangste/auth'
+import { AuthModule } from '@tsangste/nestjs-auth'
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
   imports: [AuthModule],
