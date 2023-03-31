0de6373b2cfe440e0efc1c58cb74f543592a7358
Steven Tsang <tsangste@gmail.com>
2023-03-31 23:42:01 +0100

    feat(nestjs-storage): add aws s3 support

--- libs/nestjs-storage/src/storage.module.ts
+++ libs/nestjs-storage/src/storage.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+
+import { S3Client } from '@aws-sdk/client-s3'
 
 import { StorageService } from './services/storage.service'
 
 @Module({
   controllers: [],
-  providers: [StorageService],
-  exports: [StorageService],
+  providers: [
+    {
+      provide: S3Client,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return new S3Client({
+          region: config.get<string>('AWS_REGION', 'eu-west-2'),
+          credentials: {
+            accessKeyId: config.get<string>('AWS_ACCESS_KEY_ID', ''),
+            secretAccessKey: config.get<string>('AWS_SECRET_ACCESS_KEY', '')
+          }
+        })
+      }
+    },
+    StorageService],
+  exports: [S3Client, StorageService]
 })
 export class StorageModule {}
