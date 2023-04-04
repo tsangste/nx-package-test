ec1d44f6b8980a62a0d119b9d474fd75d5339d21
Steven Tsang <tsangste@gmail.com>
2023-04-04 22:16:39 +0100

    feat(nestjs-store): code edit :)

--- libs/nestjs-storage/src/services/storage.service.spec.ts
+++ libs/nestjs-storage/src/services/storage.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 
+import { S3Client } from '@aws-sdk/client-s3'
+
 import { StorageService } from './storage.service'
 
 describe('StorageService', () => {
@@ -7,7 +9,13 @@ describe('StorageService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StorageService],
+      providers: [
+        StorageService,
+        {
+          provide: S3Client,
+          useValue: {}
+        }
+      ],
     }).compile()
 
     service = module.get<StorageService>(StorageService)
--- libs/nestjs-storage/src/services/storage.service.ts
+++ libs/nestjs-storage/src/services/storage.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common'
 
+import { S3Client } from '@aws-sdk/client-s3'
+
 @Injectable()
-export class StorageService {}
+export class StorageService {
+  constructor(private readonly s3client: S3Client) {}
+}
