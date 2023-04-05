ad8c158a58be88cbc104595f65d77cf4fd3b3b45
Steven Tsang <tsangste@gmail.com>
2023-04-05 15:10:46 +0100

    feat(nestjs-store): upload edit :)

--- libs/nestjs-storage/src/services/storage.service.ts
+++ libs/nestjs-storage/src/services/storage.service.ts
@@ -5,4 +5,8 @@ import { S3Client } from '@aws-sdk/client-s3'
 @Injectable()
 export class StorageService {
   constructor(private readonly s3client: S3Client) {}
+
+  upload() {
+    console.log('upload files!')
+  }
 }
