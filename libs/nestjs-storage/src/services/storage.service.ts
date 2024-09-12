709debf8887142f6f620c57ce9519e5b4ad346db
Steven Tsang <tsangste@gmail.com>
2024-09-12 14:38:43 +0100

    feat: add more logging (#338)

--- libs/nestjs-storage/src/services/storage.service.ts
+++ libs/nestjs-storage/src/services/storage.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable, Logger, LoggerService } from '@nestjs/common'
 
 import { S3Client } from '@aws-sdk/client-s3'
 
 @Injectable()
 export class StorageService {
-  constructor(private readonly s3client: S3Client) {}
+  constructor(@Inject(Logger) private readonly logger: LoggerService, private readonly s3client: S3Client) {}
 
   upload() {
-    console.log('upload files!')
+    this.logger.debug('upload files!')
   }
 }
